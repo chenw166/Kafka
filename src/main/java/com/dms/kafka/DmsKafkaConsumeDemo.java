7028733ccc78b650b51c973b6b6ea9b0c410a6e4
chenw166 <chenw166@qq.com>
2019-03-25 18:02:16 +0800

    生产者同步/异步提交

--- src/main/java/com/dms/kafka/DmsKafkaConsumeDemo.java
+++ src/main/java/com/dms/kafka/DmsKafkaConsumeDemo.java
@@ -11,6 +11,8 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,55 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("DmsKafkaConsume")
 public class DmsKafkaConsumeDemo
 {
+	public Logger logger = LoggerFactory.getLogger(DmsKafkaConsumeDemo.class);
+	
+	
+	@RequestMapping("runKafkaConsume0")
+    public void runKafkaConsume0() throws IOException
+    {
+        Properties consumerConfig = Config.getConsumerConfig();
+
+        consumerConfig.put("ssl.truststore.location", Config.getTrustStorePath());
+        System.setProperty("java.security.auth.login.config", Config.getSaslConfig());
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(consumerConfig);
+        TopicPartition p = new TopicPartition(consumerConfig.getProperty("topic"),0);
+        kafkaConsumer.assign(Arrays.asList(p));
+        while(true){
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(60000);
+            for (ConsumerRecord<String, String> record : records) {
+            	String msg="Received message: (" + record.key() + ", " + record.value() + ") at partition "+record.partition()+" offset " + record.offset();
+                logger.info(msg);
+            }
+            kafkaConsumer.commitSync();
+            kafkaConsumer.close();
+            break;
+        }
+    }
+	
+	@RequestMapping("runKafkaConsume1")
+    public void runKafkaConsume1() throws IOException
+    {
+        Properties consumerConfig = Config.getConsumerConfig();
+
+        consumerConfig.put("ssl.truststore.location", Config.getTrustStorePath());
+        System.setProperty("java.security.auth.login.config", Config.getSaslConfig());
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(consumerConfig);
+        TopicPartition p = new TopicPartition(consumerConfig.getProperty("topic"),1);
+        kafkaConsumer.assign(Arrays.asList(p));
+        while(true){
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(60000);
+            for (ConsumerRecord<String, String> record : records) {
+            	String msg="Received message: (" + record.key() + ", " + record.value() + ") at partition "+record.partition()+" offset " + record.offset();
+                logger.info(msg);
+            }
+            kafkaConsumer.commitSync();
+            kafkaConsumer.close();
+            break;
+        }
+    }
+	
 	
 	@RequestMapping("runKafkaConsume")
     public void runKafkaConsume() throws IOException
--- src/main/java/com/dms/kafka/DmsKafkaProduceDemo.java
+++ src/main/java/com/dms/kafka/DmsKafkaProduceDemo.java
@@ -1,14 +1,18 @@
 package com.dms.kafka;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("DmsKafkaProduce")
 public class DmsKafkaProduceDemo
 {
+	public Logger logger = LoggerFactory.getLogger(DmsKafkaConsumeDemo.class);
 	@RequestMapping("runKafkaProduce")
     public  void runKafkaProduce() throws IOException
     {
@@ -27,15 +32,22 @@ public class DmsKafkaProduceDemo
         Producer<String, String> producer = new KafkaProducer<>(producerConfig);
         for (int i = 0; i < 10; i++)
         {
+        	int partition=0;
+        	String msg="hello, dms kafka.,this is msg:"+i;
+        	if(i%2==0){
+        		partition=0;
+        	}else{
+        		partition=1;
+        	}
             Future<RecordMetadata> future =
                 producer.send(new ProducerRecord<String, String>(
                         producerConfig.getProperty("topic"),
-                        null, "hello, dms kafka."));
+                        partition,partition+"", msg));
             RecordMetadata rm;
             try
             {
                 rm = future.get();
-                System.out.println("Succeed to send msg: " + rm.offset());
+                logger.info("Succeed to send msg: " + msg);
             }
             catch (InterruptedException | ExecutionException e)
             {
@@ -44,4 +56,79 @@ public class DmsKafkaProduceDemo
         }
         producer.close();
     }
+	
+	@RequestMapping("runKafkaProduceSynch")
+    public  void runKafkaProduceSynch() throws IOException
+    {
+        Properties producerConfig = Config.getProducerConfig();
+
+        producerConfig.put("ssl.truststore.location", Config.getTrustStorePath());
+        System.setProperty("java.security.auth.login.config", Config.getSaslConfig());
+
+        Producer<String, String> producer = new KafkaProducer<>(producerConfig);
+        logger.info("start runKafkaProduceSynch sendMsg:"+new Date());
+        for (int i = 0; i < 10000; i++)
+        {
+        	int partition=0;
+        	String msg="hello, dms kafka.,this is synchMsg:"+i;
+        	if(i%2==0){
+        		partition=0;
+        	}else{
+        		partition=1;
+        	}
+            Future<RecordMetadata> future =
+                producer.send(new ProducerRecord<String, String>(
+                        producerConfig.getProperty("topic"),
+                        partition,partition+"", msg));
+            RecordMetadata rm;
+            try
+            {
+                rm = future.get();
+                logger.info("Succeed to send msg: " + msg);
+            }
+            catch (Exception e)
+            {
+            	logger.info(e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        logger.info("end runKafkaProduceSynch sendMsg:"+new Date());
+        producer.close();
+    }
+	
+	@RequestMapping("runKafkaProduceAsynch")
+    public  void runKafkaProduceAsynch() throws IOException
+    {
+        Properties producerConfig = Config.getProducerConfig();
+
+        producerConfig.put("ssl.truststore.location", Config.getTrustStorePath());
+        System.setProperty("java.security.auth.login.config", Config.getSaslConfig());
+        Producer<String, String> producer = new KafkaProducer<>(producerConfig);
+        logger.info("start runKafkaProduceAsynch sendMsg:"+new Date());
+        for (int i = 0; i < 10000; i++)
+        {
+        	int partition=0;
+        	String msg="hello, dms kafka.,this is asynchMsg:"+i;
+        	if(i%2==0){
+        		partition=0;
+        	}else{
+        		partition=1;
+        	}
+            producer.send(new ProducerRecord<String, String>(
+                    producerConfig.getProperty("topic"),
+                    partition,partition+"", msg),new Callback() {
+						@Override
+						public void onCompletion(RecordMetadata metadata, Exception e) {
+							 //如果 Kafka 返回一个错误，onCompletion 方法会抛出一个非空（non null）异常
+		            	    if (e!= null) {
+		            	    	logger.info(e.getMessage());
+		            	    	e.printStackTrace(); 
+		            	    }
+						}
+            });
+        }
+        logger.info("end runKafkaProduceAsynch sendMsg:"+new Date());
+        producer.close();
+    }
+	
 }
